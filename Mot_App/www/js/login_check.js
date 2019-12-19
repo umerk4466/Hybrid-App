7b12e195b977ceced794e253ab3db2b8c1a278ac
unknown <umerk4466@gmail.com>
2019-12-19 18:01:02 +0000

    added html, css, js for the login success, currently is emplty files and also added some login_check.js where all the security function will be befor foing to the login success page

---
+++ Mot_App/www/js/login_check.js
@@ -0,0 +1,7 @@
+// fuction to check if the user can acces the login_success page or not
+function login_check() {
+  window.location = "login_success.html";
+  console.log(
+    "before access this page please add some function to check the user if its allow"
+  );
+}
