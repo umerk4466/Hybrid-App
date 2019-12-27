a818a092a37174c3da5a268f466f30dd7caf125d
unknown <umerk4466@gmail.com>
2019-12-27 23:31:32 +0000

    added form tags int index.html and modified js of index.js

--- Mot_App/www/js/login_check.js
+++ Mot_App/www/js/login_check.js
@@ -1,7 +1,9 @@
-// fuction to check if the user can acces the home page or not
-function login_check() {
+$("#login_form").submit(function(event) {
+  alert("Handler for .submit() called.");
   window.location = "home.html";
-  console.log(
-    "before access this page please add some function to check the user if its allow"
-  );
-}
+});
+
+$("#signup_form").submit(function(event) {
+  alert("Your account created Successfully!! Please Login now");
+  window.location = "index.html";
+});
