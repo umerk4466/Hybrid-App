be4116800a3a8e203df697e9a37da20992813729
unknown <umerk4466@gmail.com>
2020-01-02 02:18:36 +0000

    added api website in ajax login and register functions and also added test apk file to check in the android

--- Mot_App/www/js/index.js
+++ Mot_App/www/js/index.js
@@ -17,45 +17,37 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        // var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
-
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
-
-        // console.log('Received Event: ' + id);
-        console.log('receivedEvent runnig in the index.js');
-
+  // Application Constructor
+  initialize: function() {
+    if (!window.navigator.onLine) {
+      window.location = "home.html";
     }
+    this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener("deviceready", this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+    app.receivedEvent("deviceready");
+  },
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    // var parentElement = document.getElementById(id);
+    // var listeningElement = parentElement.querySelector('.listening');
+    // var receivedElement = parentElement.querySelector('.received');
+
+    // listeningElement.setAttribute('style', 'display:none;');
+    // receivedElement.setAttribute('style', 'display:block;');
+
+    // console.log('Received Event: ' + id);
+    console.log("receivedEvent runnig in the index.js");
+  }
 };
-
-// hide the footer on the keybord is active.
-$("input").blur(function() {
-    $("[data-role=footer]").show();
-});
-
-$("input").focus(function() {
-    $("[data-role=footer]").hide();
-});
-
--- Mot_App/www/js/login_check.js
+++ Mot_App/www/js/login_check.js
@@ -1,9 +1,114 @@
-$("#login_form").submit(function(event) {
-  alert("Handler for .submit() called.");
-  window.location = "home.html";
+// jquery function on click
+$("#login-btn").click(function() {
+  var username = $("#login-username-inp").val();
+  var password = $("#login-password-inp").val();
+  if (username === "" || password === "") {
+    alert("Please fill all the fields");
+    changeLoginInputColor("red");
+  } else {
+    loginAjax(username, password);
+  }
+});
+$("#signup-btn").click(function() {
+  var reg_no = $("#signup-vehicle-no").val();
+  var username = $("#signup-username-inp").val();
+  var password = $("#signup-password-inp").val();
+
+  if (reg_no === "" || username === "" || password === "") {
+    alert("Please fill all the fields");
+    changeSignupInputColor("red");
+  } else {
+    signupAjax(reg_no, username, password);
+  }
 });
 
-$("#signup_form").submit(function(event) {
-  alert("Your account created Successfully!! Please Login now");
-  window.location = "index.html";
+// ****************************AJAX****************************
+// ajax function for login page
+function loginAjax(username, password) {
+  var success;
+  $.ajax({
+    url:
+      "https://motproject01.pythonanywhere.com/api/get/user/?username=" +
+      username +
+      "&password=" +
+      password,
+    dataType: "json",
+    type: "GET",
+    timeout: 25000,
+    success: function(data, status) {
+      window.location = "home.html";
+      changeLoginInputColor("default");
+    },
+    error: function() {
+      alert("Incorect Username or Password or No Internet");
+
+      changeLoginInputColor("red");
+    }
+  });
+}
+
+// ajax function for signup page
+function signupAjax(reg_no, username, password) {
+  var success;
+  $.ajax({
+    url:
+      "https://motproject01.pythonanywhere.com/api/register/user/?reg_no=" +
+      reg_no +
+      "&username=" +
+      username +
+      "&password=" +
+      password,
+    dataType: "json",
+    type: "POST",
+    timeout: 25000,
+    success: function(data, status) {
+      if (data.message === "UserAlreadyExist") {
+        alert("This Username is already taken. Try another one");
+        changeSignupInputColor("red");
+      } else if (data.message === "UserCreatedSuccessfully") {
+        alert("Account for " + username + " is Created successfully login now");
+        window.location = "index.html";
+      }
+    },
+    error: function() {
+      alert("Somethis is wrong. Try again");
+      changeSignupInputColor("red");
+    }
+  });
+}
+
+// ****************************FUNCTIONS****************************
+// hide the footer on the keybord is active.
+$("input").blur(function() {
+  $("[data-role=footer]").show();
 });
+
+$("input").focus(function() {
+  $("[data-role=footer]").hide();
+});
+
+// fucntion to change login input colors
+function changeLoginInputColor(color) {
+  if (color === "red") {
+    $("#login-username-inp").css("border", "1px solid red");
+    $("#login-password-inp").css("border", "1px solid red");
+  }
+  if (color === "default") {
+    $("#login-username-inp").css("border", "");
+    $("#login-password-inp").css("border", "");
+  }
+}
+
+// fucntion to change signup input colors
+function changeSignupInputColor(color) {
+  if (color === "red") {
+    $("#signup-username-inp").css("border", "1px solid red");
+    $("#signup-password-inp").css("border", "1px solid red");
+    $("#signup-vehicle-no").css("border", "1px solid red");
+  }
+  if (color === "default") {
+    $("#signup-username-inp").css("border", "");
+    $("#signup-password-inp").css("border", "");
+    $("#signup-vehicle-no").css("border", "");
+  }
+}
